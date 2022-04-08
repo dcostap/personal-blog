2d40ecfd1e39ac1b872cdf1b4e03ee8448dfa27b
Dario Costa Pena <Dario Costa@CASTROSUA.COM>
2022-04-08 14:33:29 +0200

    The dark to light mode switcher is working now

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,11 @@ window.onload = function () {
 }
 
 function updateTheme() {
-    // this checks whether system dark mode is set
+    // check for the OS theme setting
     let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
-    // this checks for session storage telling to override
-    // the system preferences
-    let theme = sessionStorage.getItem('theme');
 
-    console.log("os_setting_dark: " + systemInitiatedDark.matches);
+    // setting that overrides OS theme, so user can change it
+    let theme = sessionStorage.getItem('theme');
 
     if ((theme != null && theme == "dark") || (theme == null && systemInitiatedDark.matches)) {
         document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
@@ -18,6 +16,12 @@ function updateTheme() {
         document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
         sessionStorage.setItem('theme', 'light');
     }
+
+    // if the OS changes theme, we override any configuration to match it
+    systemInitiatedDark.addListener(function() {
+        sessionStorage.clear();
+        updateTheme();
+    });
 }
 
 function themeSwitcher() {
